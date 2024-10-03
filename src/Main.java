617b51bddf725513e8610d254fb2614d48884a59
hima <13523089@std.stei.itb.ac.id>
2024-10-03 20:12:22 +0700

    feat: menu

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args){
+        int command = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            while (command != 8) {
+                System.out.println("\n===== M E N U =====");
+                System.out.println("1. Sistem Persamaan Linier");
+                System.out.println("2. Determinan");
+                System.out.println("3. Matriks Balikan");
+                System.out.println("4. Interpolasi Polinom");
+                System.out.println("5. Interpolasi Bicubic Spline");
+                System.out.println("6. Regresi linier dan kuadratik berganda");
+                System.out.println("7. Interpolasi Gambar");
+                System.out.println("8. Keluar");
+                System.out.println("Pilih menu yang tersedia: ");
+                command = sc.nextInt();
+                
+                while(command < 1 || command > 8){
+                    System.out.printf("Masukan tidak valid!\nPilihan: ");
+                    command = sc.nextInt();
+                }
+                
+                switch (command) {
+                    case 1 -> {
+                        SPL.printMetodeSPL();
+                    }
+                    case 2 -> {
+                        System.out.println("2");
+                    }
+                    case 3 -> {
+                        System.out.println("3");
+                    }
+                    case 4 -> {
+                        System.out.println("4");
+                    }
+                    case 5 -> {
+                        System.out.println("5");
+                    }
+                    case 6 -> {
+                        System.out.println("6");
+                    }
+                    case 7 -> {
+                        System.out.println("7");
+                    }
+                    case 8 -> {
+                        System.out.println("See u~");
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ src/SPL.java
@@ -0,0 +1,10 @@
+public class SPL {
+    public static void printMetodeSPL() {
+		System.out.println("\n1. Metode eliminasi Gauss");
+		System.out.println("2. Metode eliminasi Gauss-Jordan");
+		System.out.println("3. Metode Matriks Balikan");
+		System.out.println("4. Kaidah Cramer");
+		System.out.println("5. Kembali");
+		System.out.println("Pilih metode penyelesaian: ");
+	}
+}
