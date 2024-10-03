44587eaf23ccf412af11f6ade4368c1a636cc44c
hima <13523089@std.stei.itb.ac.id>
2024-10-03 20:17:10 +0700

    feat: add matrix constructor

--- src/lib/Matrix.java
+++ src/lib/Matrix.java
@@ -1,3 +1,16 @@
 public class Matrix {
-    
+    double[][] mt = null;
+    int rowCnt, colCnt;
+
+    /* *** CONSTRUCTOR *** */
+    public Matrix(int x, int y) {
+		mt = new double[x][y];
+		rowCnt = x;
+		colCnt = y;
+	}
+	public Matrix() {
+		mt = new double[1][1];
+		rowCnt = 1;
+		colCnt = 1;
+	}
 }
