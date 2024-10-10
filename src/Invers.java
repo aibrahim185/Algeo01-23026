2575912794e4b7a56ae4835687b9e4bfb3db9353
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-10-10 10:40:09 +0700

    feat: add inverse

--- src/Invers.java
+++ src/Invers.java
@@ -1,8 +1,14 @@
-
+import lib.Matrix;
 import java.util.Scanner;
 
 public class Invers {
     public static void driver(Scanner sc) {
-        System.out.println("driver Invers");
+        Matrix m = new Matrix();
+        m.read(sc);
+        if (m.determinanEkspansiKofaktor()==0){
+            System.out.println("Matriks tidak mempunyai matriks balikan");
+        }
+        else{m.inverse();}
+        m.print();
     }
 }
