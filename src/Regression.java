50dbb3b67e014215543529fd98b564506dcd1d59
hima <19623185@std.stei.itb.ac.id>
2024-10-10 09:55:34 +0700

    feat: implemen regresi

--- src/Regression.java
+++ src/Regression.java
@@ -1,8 +1,59 @@
-
+import lib.*;
 import java.util.Scanner;
 
 public class Regression {
     public static void driver(Scanner sc) {
-        System.out.println("driver Regresi");
+        System.out.print("Masukkan jumlah peubah x : ");
+        int n = sc.nextInt();  
+
+        System.out.print("Masukkan jumlah data     : ");
+        int m = sc.nextInt();  
+        
+        System.out.println("Masukkan semua nilai-nilai x dan nilai y (kolom terakhir adalah y):");
+        Matrix X = new Matrix(m, n);
+        Matrix Y = new Matrix(m, 1);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                double temp = sc.nextDouble();
+                X.setMat(i, j, temp);  
+            }
+            double tempY = sc.nextDouble();
+            Y.setMat(i, 0, tempY);  
+        }
+
+        // Regresi Linier
+        Matrix resLinier = regresiLinier(X, Y);
+        System.out.println("Hasil regresi linier:");
+        resLinier.print();
+
+        // Regresi Kuadratik
+        Matrix resKuadratik = regresiKuadratik(X, Y);
+        System.out.println("Hasil regresi kuadratik:");
+        resKuadratik.print();;
+    }
+
+    public static Matrix regresiLinier(Matrix X, Matrix Y) {
+        Matrix Xt = X.transpose();  
+        Matrix XtX = Xt.mulMatrix(X);  
+        Matrix XtY = Xt.mulMatrix(Y);  
+        Matrix B = XtX.inverse().mulMatrix(XtY);  // (Xt * X)^(-1) * Xt * Y
+        return B;
+    }
+
+    public static Matrix regresiKuadratik(Matrix X, Matrix Y) {
+        int n = X.getCol();  
+        int m = X.getRow();  
+
+        Matrix X_quad = new Matrix(m, 2 * n);  
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                X_quad.setMat(i, j, X.getMat(i, j));  
+                X_quad.setMat(i, n + j, Math.pow(X.getMat(i, j), 2));  
+            }
+        }
+
+        return regresiLinier(X_quad, Y);
     }
 }
