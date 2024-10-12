f00c360a0a6c43b686e6c3731fe1ff98bb3144b3
hima <19623185@std.stei.itb.ac.id>
2024-10-12 20:12:24 +0700

    feat: add bicubic

--- src/Bicubic.java
+++ src/Bicubic.java
@@ -13,9 +13,49 @@ public class Bicubic {
                 X.setMat(i, j, temp);  
             }
         }
+        Matrix flatMatrix = flatten(X);
         double a, b;
         a = sc.nextDouble();
         b = sc.nextDouble();
+        bicubicInterpolation(flatMatrix, a, b);
     }
     
+    public static Matrix flatten(Matrix X) {
+        Matrix flatMatrix = new Matrix(16, 1); // 16x1 matrix
+        int idx = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                flatMatrix.setMat(idx, 0, X.getMat(i, j));
+                idx++;
+            }
+        }
+        return flatMatrix;
+    }
+
+    public static Matrix muli() {
+        Matrix ret = new Matrix(16, 16);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    for (int l = 0; l < 4; l++) {
+                        ret.setMat(i * 4 + j, k * 4 + l, Math.pow(i - 1, k) * Math.pow(j - 1, l));
+                    }
+                }
+            }
+        }
+        ret.print();
+        return ret.inverse();
+    }    
+    
+    public static void bicubicInterpolation(Matrix f, double tx, double ty) {
+        Matrix a = muli().mulMatrix(f);
+		double ans = 0;
+		for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                ans += a.getMat(i * 4 + j, 0) * Math.pow(tx, i) * Math.pow(ty, j);
+			}
+		}
+		muli().print();
+		System.out.println("f(" + tx + " , " + ty + ") = " + ans);
+	}
 }
