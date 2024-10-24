952f34f71ea9cd020740fc54ce515ffab4e7eab4
hima <19623185@std.stei.itb.ac.id>
2024-10-24 08:51:18 +0700

    feat: add polinom interpolate gui

--- gui/src/main/java/com/gui/matrix/Interpolate.java
+++ gui/src/main/java/com/gui/matrix/Interpolate.java
@@ -4,22 +4,19 @@ import java.util.Scanner;
 
 import com.gui.matrix.lib.Matrix;
 
-
 public class Interpolate {
     public static void driver(Scanner sc) {
         System.out.print("Masukkan jumlah data : ");
         int n = sc.nextInt();  
         
-        System.out.println("Masukkan data        : x0 x1 x2 ... xn y0 y1 y2 ... yn");
+        System.out.println("Masukkan data        : x0 y0 x1 y1 ... xn yn");
         Matrix X = new Matrix(n + 1, n + 1);
         Matrix XP = new Matrix(n, 1);
-        for (int i = 0; i <= n; i++) {
-            double temp = sc.nextDouble();
-            XP.setMat(i, 0, temp);  
-        }
         Matrix YV = new Matrix(1, 0);
         for (int i = 0; i <= n; i++) {
             double temp = sc.nextDouble();
+            XP.setMat(i, 0, temp);  
+            temp = sc.nextDouble();
             YV.setMat(0, i, temp);  
         }
         for (int i = 0; i <= n; i++) {
